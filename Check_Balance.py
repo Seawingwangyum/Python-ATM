080604b53477c487939c3dc92fe70ceccf9d2575
Siming Yang <yangsiming1999@gmai.com>
2018-04-12 14:26:00 -0700

    added Withdraw and Checkbalance buttons with escape options

--- Check_Balance.py
+++ Check_Balance.py
@@ -4,15 +4,15 @@ class check_balance:
         self.master = parent
         self.master.geometry("400x140")
         self.information_frame = Frame(self.master)
-        self.button_frame = Frame(self.master)
+    
         self.action_label = Label(self.information_frame,text="Total Balance: ")
         self.balance_amount = Label(self.information_frame, text="$XXXXXXXXXXXXXXXXXX", width=55)
-        self.back_button = Button(self.button_frame, text="Back", width=10)
+    
         self.information_frame.grid(row=0, column=0)
-        self.button_frame.grid(row=1,column=0, pady=20)
+       
         self.action_label.grid(row=0,column=0, pady=10)
         self.balance_amount.grid(row=1, column=0, padx=10)
-        self.back_button.grid(row=0,column=0, padx=10)
+
 if __name__ == "__main__":
     root = Tk()
     check_balance(root)
--- Withdraw.py
+++ Withdraw.py
@@ -1,23 +1,30 @@
 from tkinter import *
+
 class withdraw:
+
     def __init__(self, parent):
         self.master = parent
         self.master.geometry("400x140")
-
+        
         self.information_frame = Frame(self.master)
         self.button_frame = Frame(self.master)
-        self.action_label = Label(self.information_frame,text="Withdraw: ")
-        self.withdraw_amount = Entry(self.information_frame, width=63)
-        self.cancel_button = Button(self.button_frame, text="Cancel", width=10)
-        self.confirm_button = Button(self.button_frame, text="Confirm", width=10)
         
+        self.action_label = Label(self.information_frame,text="withdraw: ")
+        self.withdraw_amount = Entry(self.information_frame, width=63)
+        self.confirm_button = Button(self.master, text="Confirm", width=10)
+
+
+
         self.information_frame.grid(row=0, column=0)
-        self.button_frame.grid(row=1,column=0, pady=20)
+
         self.action_label.grid(row=0,column=0, pady=10)
         self.withdraw_amount.grid(row=1, column=0, padx=10)
-        self.cancel_button.grid(row=0,column=0, padx=10)
-        self.confirm_button.grid(row=0, column=1, padx=10)
+        self.button_frame.grid(row=1,column=0, pady=20)
+        self.confirm_button.grid(row=1, column=0, padx=50, sticky=E)
+
+    
+
 if __name__ == "__main__":
     root = Tk()
-    withdraw(root)
+    withdraw(root, "hello")
     mainloop()
